94ad9711a6bfdc18ec3d8977a3fd074c2b4aada3
Mohammed Musthafa A <119840348+ceo-zandora@users.noreply.github.com>
2024-06-11 00:59:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ def main():
         f.write('lnk.zanx.me')
 
     for link in links:
-        html_document = html.format(url=link['url'])
-        file_path = f"dist/{link['name']}.html"
+        html_document = html.format(url=link['originalUrl'])
+        file_path = f"dist/{link['shortUrl']}.html"
 
         with open(file_path, 'w') as f:
             f.write(html_document)
